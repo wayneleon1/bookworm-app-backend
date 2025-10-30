466ce5702f3798bdc5e0bfaddaa0455eab0bdf51
wayneleon1 <rurangwaleon10@gmail.com>
2025-10-30 22:52:58 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Serve is running on port ${PORT}`);
+});
