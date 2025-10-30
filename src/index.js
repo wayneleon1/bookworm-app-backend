f3e56ab7d263e4376faad774230e85eba800946e
wayneleon1 <rurangwaleon10@gmail.com>
2025-10-30 23:14:02 +0200

    connecting mongoose db

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Serve is running on port ${PORT}`);
+  connectDB();
 });
